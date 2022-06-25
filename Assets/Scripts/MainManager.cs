854d9b6dc4fa615a5e78a20048907c3359781360
Adrien Syrotnik <adrien.syrotnik@gmail.com>
2022-06-26 00:53:27 +0200

    Add saving data between sessions

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -13,20 +14,21 @@ public class MainManager : MonoBehaviour
     public Text ScoreText;
     public Text bestScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        LoadScore();
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -71,7 +73,46 @@ public class MainManager : MonoBehaviour
 
     public void GameOver()
     {
+        SaveScore();
         m_GameOver = true;
         GameOverText.SetActive(true);
+    }
+
+
+    private void SaveScore()
+    {
+        PlayerScore newPlayerScore = new PlayerScore();
+        newPlayerScore.userName = DataManager.Instance.userName;
+        newPlayerScore.score = m_Points;
+
+        if(newPlayerScore.score > bestGlobalScore)
+        {
+            string json = JsonUtility.ToJson(newPlayerScore);
+            File.WriteAllText(Application.persistentDataPath + "/bestScore.json", json);
+        }
+        
+        
+    }
+
+    private int bestGlobalScore = 0;
+
+    private void LoadScore()
+    {
+        string pathBestScore = Application.persistentDataPath + "/bestScore.json";
+        if (File.Exists(pathBestScore))
+        {
+            string json = File.ReadAllText(pathBestScore);
+            PlayerScore bestScore = JsonUtility.FromJson<PlayerScore>(json);
+            bestGlobalScore = bestScore.score;
+            bestScoreText.text = "Best Score : " + bestScore.userName + " : " + bestScore.score;
+        }
+
+    }
+
+    [System.Serializable]
+    class PlayerScore
+    {
+        public int score;
+        public string userName;
     }
 }
--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
+using System.IO;
 
 public class MenuUI : MonoBehaviour
 {
@@ -18,7 +19,7 @@ public class MenuUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadScore();
     }
 
     // Update is called once per frame
@@ -40,5 +41,24 @@ public class MenuUI : MonoBehaviour
     #else
         Application.Quit();
     #endif
+    }
+
+    private void LoadScore()
+    {
+        string pathBestScore = Application.persistentDataPath + "/bestScore.json";
+        if (File.Exists(pathBestScore))
+        {
+            string json = File.ReadAllText(pathBestScore);
+            PlayerScore bestScore = JsonUtility.FromJson<PlayerScore>(json);
+            scoreText.text = "Best Score : " + bestScore.userName + " : " + bestScore.score;
+        }
+
+    }
+
+    [System.Serializable]
+    class PlayerScore
+    {
+        public int score;
+        public string userName;
     }
 }
